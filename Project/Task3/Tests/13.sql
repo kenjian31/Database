49a70deb7aaf19e84ce95841376b6aaad17b9f7a
wang8635 <wang8635@umn.edu>
2019-12-13 03:00:02 -0600

    Add files via upload

---
+++ Project/Task3/Tests/13.sql
@@ -0,0 +1,17 @@
+insert into NURSE
+values ('121', 'Charle', 'C', 'Andrew', '12345678');
+
+insert into ASSESSMENT_DEPARTMENT
+values ('121', 'Charle', 'C', 'Andrew', '12345678');
+
+insert into ASSESSMENT_NURSE
+values ('121', 'Charle', 'C', 'Andrew', '12345678');
+
+insert into VISIT
+values ('113', '400', '2019-12-11', '1','121', '121','400','13546556', 'form1');
+
+insert into VISIT
+values ('120', '400', '2019-12-11', '1','1', '121','400','13546556', 'form1');
+
+select *
+from VISIT;
